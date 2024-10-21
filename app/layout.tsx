fa9f6360736ed051789ade86f6e970816b1bfa99
Viktor Holovin <holovin@mail.com>
2024-10-21 11:55:51 +0200

    Change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "holovin.com",
+    template: "%s | holovin.com",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "My curriculum",
   openGraph: {
-    title: "chronark.com",
+    title: "holovin.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://holovin.com",
+    siteName: "holovin.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://holovin.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "holovin",
     card: "summary_large_image",
   },
   icons: {
